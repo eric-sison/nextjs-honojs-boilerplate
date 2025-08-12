3034e5bb79353b69b199b2d9d4185c89201f570e
eric-sison <ericsison.dev@gmail.com>
2025-08-12 10:31:39 +0800

    added allowedDevOrigins config option for future next versions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"], // include domain names and/or IP addresses for local dev server
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
