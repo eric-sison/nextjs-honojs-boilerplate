c4c23dd483714f8e8241dc3d23168801988a55ba
eric-sison <ericsison.dev@gmail.com>
2025-08-28 12:29:47 +0800

    updated required env variable for application host

--- utils/validators/common.ts
+++ utils/validators/common.ts
@@ -3,7 +3,7 @@ import z from "zod";
 // zod validation schemas
 export const EnvSchema = z.object({
   // Add environment variables here...
-  NEXT_PUBLIC_HOST: z.url(),
+  APP_HOST: z.url(),
   DB_HOST: z.string(),
   DB_PORT: z.coerce.number(),
   DB_USER: z.string(),
