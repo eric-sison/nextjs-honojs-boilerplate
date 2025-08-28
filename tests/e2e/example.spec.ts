7b398ef2e4666a778d7c9769a05ceb004e9a2137
eric-sison <ericsison.dev@gmail.com>
2025-08-28 15:22:37 +0800

    chore: fixed e2e sample test case

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -4,5 +4,5 @@ test("has title", async ({ page }) => {
   await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/NextJS Boilerplate/);
+  await expect(page).toHaveTitle(/Full Stack NextJS Boilerplate/);
 });
