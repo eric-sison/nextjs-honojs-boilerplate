6736841881994ecf322c735981db8956a653a827
eric-sison <ericsison.dev@gmail.com>
2025-09-30 15:59:05 +0800

    refactor: removed unsued default variable

--- shared/validators/env.ts
+++ shared/validators/env.ts
@@ -3,7 +3,6 @@ import z from "zod";
 // zod validation schemas
 export const EnvSchema = z.object({
   // Add environment variables here...
-  APP_HOST: z.url(),
   DB_HOST: z.string(),
   DB_PORT: z.coerce.number(),
   DB_USER: z.string(),
